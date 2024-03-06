44dd21a50d58b7745f94b5d64e61a868c37845d1
KALEEMULLAH <160345037+Kaleemullah3095@users.noreply.github.com>
2024-03-06 23:51:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ["ALI", "JANI", "ASIF", "SONO", "MITHO"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let names:string[]=["ALI","JANI","ASIF","SONO","MITHO"];
+for (let i=0; i<names.length;i++){
+    console.log(names[i]);
+}
